020721d89d76f197c7f334cd4f77b965ffa1c161
Nicholas Adams <njadams.722@gmail.com>
2022-10-07 10:59:36 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const clay =["Clay", "Clay", "Clay", "Clay"]
+const toFireInKiln=[]
+
+for (const chunks of clay) {
+    const mug=`${chunks} coffee mug`
+    toFireInKiln.push(mug)
+    
+}
+
+console.log(toFireInKiln)
